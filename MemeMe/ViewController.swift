a4ff84382a08d8553a9553f606547e5c9d8a2331
priferr <priferr@gmail.com>
2022-08-08 11:34:24 -0300

    First step of MemeMe App

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let chosenImage = info[.originalImage] as? UIImage {
+                imagePickerView.image = chosenImage
+                dismiss(animated: true, completion: nil)
+                //shareBarButtonItem.isEnabled = true
+        }
+    }
 }
 
