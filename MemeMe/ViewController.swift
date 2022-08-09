acf05cc924406afde8b4fa155bf1df44ad1816c1
priferr <priferr@gmail.com>
2022-08-09 13:15:04 -0300

    Privacy settings | Text fields

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,36 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate {
+UINavigationControllerDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     
+    //MARK: Text Field Properties
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textFieldProperty()
     }
+    
+    func textFieldProperty() {
+        topTextField.text = "TOP"
+        topTextField.textAlignment = .center
+        topTextField.delegate = self
+       // topTextField.defaultTextAttributes = memeTextAttributes
+        
+        bottomTextField.text = "BOTTOM"
+        bottomTextField.textAlignment = .center
+        bottomTextField.delegate = self
+        //bottomTextField.defaultTextAttributes = memeTextAttributes
+    }
+    
+    @IBAction func topTextFieldClears(_ sender: UITextField) {
+        topTextField.text = ""
+    }
+    
+    @IBAction func bottomTextFieldClears(_ sender: UITextField) {
+        bottomTextField.text = ""
+    }
+    
+    
+    //let memeTextAttributes: [NSAttributedString.Key: Any] = [
+    //    NSAttributedString.Key.strokeColor: /* TODO: fill in //appropriate UIColor */,
+     //   NSAttributedString.Key.foregroundColor: /* TODO: fill in //appropriate UIColor */,
+      //  NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+     //   NSAttributedString.Key.strokeWidth:  /* TODO: fill in //appropriate Float */
+  //  ]
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
     //MARK: Pick An Image
-    
-    func pickAnImage(source: UIImagePickerController.SourceType) {
+
+    @IBAction func PickImagePhoto(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBAction func photoButton(_ sender: Any) {
-        pickAnImage(source: .photoLibrary)
+    @IBAction func PickImageCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBAction func cameraButton(_ sender: Any) {
-        pickAnImage(source: .camera)
-    }
-    
+    //MARK: Return Chosen Image
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let chosenImage = info[.originalImage] as? UIImage {
                 imagePickerView.image = chosenImage
